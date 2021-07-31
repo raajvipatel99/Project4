121f5ec58f9e1dcff16b99ba032b963ef4735bcb
raajvipatel99 <raajvipatel99@gmail.com>
2021-07-30 20:31:35 -0400

    Task: added redis session mgmt

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from typing import List, Dict
+
+import redis
 import simplejson as json
-from flask import Flask, request, Response, redirect, jsonify
+from flask import Flask, request, Response, redirect, jsonify, session, render_template_string, url_for
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Session
 from wtforms import StringField, SubmitField
 from flask_wtf import FlaskForm
 
@@ -154,6 +157,50 @@ def api_delete(biostat_id) -> str:
     return biostat
 
 db.create_all();
+
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
+
+# Create and initialize the Flask-Session object AFTER `app` has been configured
+server_session = Session(app)
+
+
+@app.route('/set_email', methods=['GET', 'POST'])
+def set_email():
+    if request.method == 'POST':
+        # Save the form data to the session object
+        session['email'] = request.form['email_address']
+        return redirect(url_for('get_email'))
+
+    return """
+        <form method="post">
+            <label for="email">Enter your email address:</label>
+            <input type="email" id="email" name="email_address" required />
+            <button type="submit">Submit</button
+        </form>
+        """
+
+
+@app.route('/get_email')
+def get_email():
+    return render_template_string("""
+            {% if session['email'] %}
+                <h1>Welcome {{ session['email'] }}!</h1>
+            {% else %}
+                <h1>Welcome! Please enter your email <a href="{{ url_for('set_email') }}">here.</a></h1>
+            {% endif %}
+        """)
+
+
+@app.route('/delete_email')
+def delete_email():
+    # Clear the email stored in the session object
+    session.pop('email', default=None)
+    return '<h1>Session deleted!</h1>'
+
+
 
 
 if __name__ == '__main__':
